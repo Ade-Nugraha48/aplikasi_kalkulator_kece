f757a557514c0a34f1b8a1b02af9b45e0703bb9a
Ade-Nugraha48 <adenugi1011@gmail.com>
2026-09-19 15:49:47 +0700

    Pembersihan Dosa

--- test_script.dart
+++ test_script.dart
@@ -1,1 +1,14 @@
-import 'lib/features/calculator/models/big_decimal.dart'; void main() { var a = BigDecimal.tryParse('124240190.124240191'); var b = BigDecimal.tryParse('-124240192'); print('a = \'); print('b = \'); print('a * b = \'); print('formatted = \'); }
+import 'lib/models/big_decimal.dart';
+
+void main() {
+  var a = BigDecimal.tryParse('124240190.124240191');
+  var b = BigDecimal.tryParse('-124240192');
+  
+  print('a = $a');
+  print('b = $b');
+  
+  if (a != null && b != null) {
+    print('a * b = ${a * b}');
+    print('formatted = ${(a * b).toFormattedString()}');
+  }
+}
